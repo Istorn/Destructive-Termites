4eeca24b015f7ec401ecf26c83b19e6fc290235e
Mauro <mauro.caps92@hotmail.it>
2015-11-20 23:24:40 +0100

    Pseudo-Human, Graph e XML load

---
+++ DestructiveTermites/Assets/Scripts/Graph.cs
@@ -0,0 +1,182 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//Classe Singleton condivisa da tutti gli oggetti per in movimento per gestire gli spostamenti autonomi
+public static class Graph {
+
+    //Lista dei nodi di riferimento nella mappa
+    public static List<Node> nodes = new List<Node>();
+
+    //Aggiunge un nodo al grafo
+    public static void addNode(int number, Vector3 coordinates, int type)
+    {
+        Node node = new Node(number, coordinates, type);
+        nodes.Add(node);
+    }
+
+    //Aggiunge un collegamento pesato tra due nodi nel grafo
+    public static void addLink(int nodeNumber1, int nodeNumber2, int distance)
+    {
+        Node node1 = null;
+        Node node2 = null;
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            Node node = nodes[i];
+            if (node.number == nodeNumber1)
+                node1 = node;
+            else
+                if (node.number == nodeNumber2)
+                    node2 = node;
+        }
+        node1.addNeighbor(new Neighbor(node2, distance));
+        node2.addNeighbor(new Neighbor(node1, distance));
+    }
+
+    //Dato un identificativo per il nodo, lo cerca nel grafo e restituisce il nodo stesso
+    public static Node findNode(int number)
+    {
+        foreach(Node n in nodes)
+            if (number == n.number)
+                return n;
+        return null;
+    }
+    
+    //Resetta il grafo, cancellandone nodi e link
+    public static void reset()
+    {
+        foreach (Node node in nodes)
+        {
+            node.neighbors.Clear();
+        }
+        nodes.Clear();
+    }
+
+    //Restituisce il percordo minimo (Calcolato tramite l'algoritmo di Dijkstra) tra due nodi
+    public static List<Node> getPath(int startNodeNumber, int endNodeNumber)
+    {
+
+        //Recupero il nodo iniziale e quello finale
+        Node u = findNode(startNodeNumber);                                 
+        Node endNode = findNode(endNodeNumber);
+
+        //Definisco gli array per le distanze e i nodi precedenti
+        double[] distances = new double[nodes.Count];
+        Node[] previous = new Node[nodes.Count];
+        
+        //Li inizializzo
+        foreach (Node n in nodes)                                           
+        {
+            // Distanza iniziale sconosciuta dalla sorgente a v
+            distances[n.number] = double.PositiveInfinity;
+            // Nodo precedente in un percorso ottimale dalla sorgente    
+            previous[n.number] = null;                                     
+        }
+
+        // Distanza dalla sorgente alla sorgente
+        distances[startNodeNumber] = 0;
+
+        // Tutti i nodi nel grafo sono non ottimizzati e quindi stanno in Q
+        List<Node> Q = new List<Node>(nodes);                               
+
+        while (Q.Count > 0)                                                 
+        {
+            //All'inizio uso u = startNode, poi cerco il vertice in Q con minole dist[]
+            if (u == null)
+            {
+                u = Q[0];
+                double minDist = (double)distances[u.number];
+                foreach (Node n in Q)                                        
+                {
+                    double dist = (double)distances[n.number];
+                    if (dist < minDist)
+                    {
+                        minDist = dist;
+                        u = n;
+                    }
+                }
+            }
+            //Se il nodo con minore distanza è il nodo finale termino
+            if (u.number == endNode.number)
+                break;
+            else
+            {
+                //Rimuovo u da Q
+                Q.Remove(u);
+                //Se tutti i vertici rimanenti sono inaccessibili dal nodo sorgente termino
+                if (distances[u.number] == double.PositiveInfinity)
+                    break;                                                       
+                //Ricalcolo le distanze dei vicini di u
+                foreach (Neighbor v in u.neighbors)                               
+                    if (Q.IndexOf(v.node) >= 0)
+                    {
+                        double alt = distances[u.number] + u.getDistance(v.node);
+                        if (alt < distances[v.node.number])                          
+                        {
+                            distances[v.node.number] = alt;
+                            previous[v.node.number] = u;
+                            //decrease-key v in Q;                                     // Riordina v nella coda
+                        }
+                    }
+            }
+            u = null;
+        }
+        u = endNode;
+        //Calcolo il percorso scandando Previuous[] al rovescio
+        List<Node> path = new List<Node>();
+        while (previous[u.number] != null)                                   
+        {
+            path.Insert(0, u);
+            u = previous[u.number];                                         
+        }
+        path.Insert(0, findNode(startNodeNumber));
+        return path;
+    }
+
+    //Classe Nodo
+    public class Node
+    {
+        public int number;
+        public int type;
+        public Vector3 coordinates;
+        public List<Neighbor> neighbors;
+
+        //Costruttore
+        public Node(int number, Vector3 coordinates, int type)
+        {
+            this.number = number;
+            this.coordinates = coordinates;
+            this.type = type;
+            neighbors = new List<Neighbor>();
+        }
+
+        //Aggiunge un vicino al nodo
+        public void addNeighbor(Neighbor neighbor)
+        {
+            neighbors.Add(neighbor);
+        }
+
+        //Recupera la distanza tra il nodo e un dato vicino
+        public int getDistance(Node node)
+        {
+            foreach (Neighbor n in neighbors)
+                if (n.node.number == node.number)
+                    return n.distance;
+            return 0;
+        }
+    }
+
+    //Classe per la gestione dei nodi vicini ad un altro
+    public class Neighbor
+    {
+        public Node node;
+        public int distance;
+
+        //Costruttore
+        public Neighbor(Node neighbor, int distance)
+        {
+            this.node = neighbor;
+            this.distance = distance;
+        }
+    }
+}
---
+++ DestructiveTermites/Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+    Animator animator;
+    IEnumerator Start()
+    {
+        
+        animator = GetComponent<Animator>();
+
+        LevelData.provaXML("Assets/Levels/level1.xml");
+
+        //Graph graph = Costanti.generateGraph();
+
+        List<Graph.Node> path = Graph.getPath(0, 3);
+        //path.AddRange(graph.getPath(2, 4));
+
+        while (path.Count > 1)
+        {
+            Graph.Node start = path[0];
+            Graph.Node end = path[1];
+
+            
+            
+            int sign = System.Math.Sign(end.coordinates.x - start.coordinates.x);
+
+            transform.localScale = new Vector3(System.Math.Abs(transform.localScale.x) * sign, transform.localScale.y, transform.localScale.z);
+            path.RemoveAt(0);
+            yield return  StartCoroutine(MoveObject(transform, start, end, LevelData.HUMAN_SPEED));
+            
+        }
+        yield return null;
+    }
+
+    IEnumerator MoveObject(Transform thisTransform, Graph.Node start, Graph.Node end, float time)
+    {
+        //ann.Play("Walking");
+        //if (end.type == 1)
+            animator.SetBool("isOnStairs", end.type == 1);
+       // else
+        //    animator.SetBool("isOnStairs", end.type == 1);
+        //an.SetBool("isOnStairs", end.type == 1);
+        //Debug.Log(an.GetBool("isOnStairs").ToString());
+        //an.enabled = false;
+        var i = 0.0f;
+        var rate = 1.0f / time;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            thisTransform.position = Vector3.Lerp(start.coordinates, end.coordinates, i);
+            yield return null;
+        }
+    }
+
+   void OnCollisionEnter2D(Collision2D collision) {
+
+       if (collision.gameObject.tag == "LiveObject")
+       {
+           Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+       }
+ 
+    }
+
+    /*void PlayAnimation(string animName) {
+    if (!animation.IsPlaying(animName))
+        animation.CrossFadeQueued(animName, 0.3f, QueueMode.PlayNow);
+}*/
+}
