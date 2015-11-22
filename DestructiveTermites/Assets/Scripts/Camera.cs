da9d421f7c1c4f953857cb62250d8f745d3e6931
Mauro <mauro.caps92@hotmail.it>
2015-11-22 18:29:32 +0100

    Inizio gestione movimento telecamera con tasti

---
+++ DestructiveTermites/Assets/Scripts/Camera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+    private float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(this);
+	}
+	
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        transform.position = new Vector3(
+           Mathf.Clamp(transform.position.x, -5, 5),
+           Mathf.Clamp(transform.position.y, -5, 5),
+           Mathf.Clamp(transform.position.z, -10, 10));
+    }
+
+ 
+}
