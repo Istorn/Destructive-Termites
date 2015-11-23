3b8f41bacd8774c8a1ec854a4a71db9a25df7508
Mauro <mauro.caps92@hotmail.it>
2015-11-23 20:04:28 +0100

    Fix camera translation

--- DestructiveTermites/Assets/Scripts/Camera.cs
+++ DestructiveTermites/Assets/Scripts/Camera.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Camera : MonoBehaviour {
 
-    private float speed = 5.0f;
+    private float speed = 3.0f;
 	// Use this for initialization
 	void Start () {
         DontDestroyOnLoad(this);
@@ -14,25 +14,27 @@ public class Camera : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            moveCamera(new Vector3(speed * Time.deltaTime, 0, 0));
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+            moveCamera(new Vector3(-speed * Time.deltaTime, 0, 0));
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            moveCamera(new Vector3(0, -speed * Time.deltaTime, 0));
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            moveCamera(new Vector3(0, speed * Time.deltaTime, 0));
         }
-        transform.position = new Vector3(
-           Mathf.Clamp(transform.position.x, -5, 5),
-           Mathf.Clamp(transform.position.y, -5, 5),
-           Mathf.Clamp(transform.position.z, -10, 10));
     }
 
- 
+    private void moveCamera(Vector3 newPosition)
+    {
+       transform.Translate(new Vector3(
+           Mathf.Clamp(newPosition.x, -5, 5),
+           Mathf.Clamp(newPosition.y, -5, 5),
+           Mathf.Clamp(newPosition.z, -10, 10)));
+    }
 }
