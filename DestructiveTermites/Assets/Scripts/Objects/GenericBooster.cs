327590bd8e2bd733c9e98f8ea41b3f6dada02c27
LydiaC <lydia.cilie@gmail.com>
2015-12-02 21:36:19 +0100

    booster
    creazione del GenericBooster, da sistemare con booster e vedere come lo
    si lega con un oggetto

---
+++ DestructiveTermites/Assets/Scripts/Objects/GenericBooster.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public class GenericBooster : MonoBehaviour {
+{
+		private string Name = "";
+		private string Description = "";
+		private const timeDuration = 0;
+		private int remainingTime = 0;
+		private bool isHiddenInObject;
+		// servono le coordinate del power up?
+
+		public GenericBooster (GenericObject attachedObj){
+			this.isHiddenInObject = true;
+			// servono le coordinate dell'oggetto per settare quelle del power up?
+
+		}
+}
+
