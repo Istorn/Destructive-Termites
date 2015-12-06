9bd632276a3fa1a390e7bc27b6199a1ec7668176
Istorn <istorn.one@gmail.com>
2015-12-06 19:22:10 +0100

    Infobar script adjust

--- Assets/Scripts/GUI/Infobar.cs
+++ Assets/Scripts/GUI/Infobar.cs
@@ -12,6 +12,9 @@ public class Infobar : MonoBehaviour {
     public void setlevelPlay(Level levelToSet)
     {
         this.levelInPlay = levelToSet;
+        this.transform.Find("Background/MaterialText").GetComponent<Text>().text = "TERMITES AVAILABLE: " + this.levelInPlay.availableTermites;
+        this.transform.Find("Background/TermitesText").GetComponent<Text>().text = "";
+        this.transform.Find("Background/IntegrityText").GetComponent<Text>().text = "";
     }
     public Level getlevelPlay()
     {
@@ -20,7 +23,7 @@ public class Infobar : MonoBehaviour {
     //load at the starting of play
 	void Awake()
     {
-        this.transform.Find("Background/MaterialText").GetComponent<Text>().text = "TERMITES AVAILABLE: "+this.levelInPlay.availableTermites;
+        this.transform.Find("Background/MaterialText").GetComponent<Text>().text = "";
         this.transform.Find("Background/TermitesText").GetComponent<Text>().text = "";
         this.transform.Find("Background/IntegrityText").GetComponent<Text>().text = "";
     }
