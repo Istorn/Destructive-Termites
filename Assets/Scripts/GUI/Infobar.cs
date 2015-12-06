bcfadf08bab9b5af937e3b998fa9e7ca86af3e1f
Istorn <istorn.one@gmail.com>
2015-12-06 19:06:07 +0100

    Testing infobar

--- Assets/Scripts/GUI/Infobar.cs
+++ Assets/Scripts/GUI/Infobar.cs
@@ -4,15 +4,23 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using UnityEngine.UI;
 public class Infobar : MonoBehaviour {
-  
+    Level levelInPlay = null;
 	// Use this for initialization
 	void Start () {
 	
 	}
+    public void setlevelPlay(Level levelToSet)
+    {
+        this.levelInPlay = levelToSet;
+    }
+    public Level getlevelPlay()
+    {
+        return this.levelInPlay;
+    }
     //load at the starting of play
 	void Awake()
     {
-       // this.transform.Find("Background/MaterialText").GetComponent<Text>().text = "TERMITES AVAILABLE: "+levelInGame.availableTermites;
+        this.transform.Find("Background/MaterialText").GetComponent<Text>().text = "TERMITES AVAILABLE: "+this.levelInPlay.availableTermites;
         this.transform.Find("Background/TermitesText").GetComponent<Text>().text = "";
         this.transform.Find("Background/IntegrityText").GetComponent<Text>().text = "";
     }
@@ -29,9 +37,9 @@ public class Infobar : MonoBehaviour {
 
     }
     //deselecting an object reload main infos
-    public void deselected(Level levelInGame)
+    public void deselected()
     {
-        this.transform.Find("Background/MaterialText").GetComponent<Text>().text = "TERMITES AVAILABLE: " + levelInGame.availableTermites;
+        this.transform.Find("Background/MaterialText").GetComponent<Text>().text = "TERMITES AVAILABLE: " + this.levelInPlay.availableTermites;
         this.transform.Find("Background/TermitesText").GetComponent<Text>().text = "";
         this.transform.Find("Background/IntegrityText").GetComponent<Text>().text = "";
     }
