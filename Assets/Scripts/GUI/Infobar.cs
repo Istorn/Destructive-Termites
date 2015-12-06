880c10f7d985ad82bf642fa7f14a60505e94c310
Istorn <istorn.one@gmail.com>
2015-12-06 18:55:14 +0100

    Modified scene of infobar

--- Assets/Scripts/GUI/Infobar.cs
+++ Assets/Scripts/GUI/Infobar.cs
@@ -10,7 +10,7 @@ public class Infobar : MonoBehaviour {
 	
 	}
     //load at the starting of play
-	void Awake(Level levelInGame)
+	void Awake()
     {
         this.transform.Find("Background/MaterialText").GetComponent<Text>().text = "TERMITES AVAILABLE: "+levelInGame.availableTermites;
         this.transform.Find("Background/TermitesText").GetComponent<Text>().text = "";
