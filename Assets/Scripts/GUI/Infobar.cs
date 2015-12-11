7d0982050cf93c5ff53df6477309fd0d1bdc96cf
Istorn <istorn.one@gmail.com>
2015-12-11 11:43:31 +0100

    Infobar: methods for colony's clicking

--- Assets/Scripts/GUI/Infobar.cs
+++ Assets/Scripts/GUI/Infobar.cs
@@ -33,13 +33,24 @@ public class Infobar : MonoBehaviour {
         this.transform.Find("Background/MaterialText").GetComponent<Text>().text = "";
         this.transform.Find("Background/TermitesText").GetComponent<Text>().text = "TERMITES: ";
         this.transform.Find("Background/IntegrityText").GetComponent<Text>().text = "INTEGRITY: ";
+        this.transform.Find("Background/IronImg/IronText").GetComponent<Text>().text = "0";
+        this.transform.Find("Background/GiantImg/GiantText").GetComponent<Text>().text = "0";
+        this.transform.Find("Background/MushImg/MushText").GetComponent<Text>().text = "0";
+        this.transform.Find("Background/MaskImg/MaskText").GetComponent<Text>().text = "0";
+        this.transform.Find("Background/ShieldImg/ShieldText").GetComponent<Text>().text = "0";
+        this.transform.Find("Background/QueenImg/QueenText").GetComponent<Text>().text = "0";
+        //splitter is not visible
+        this.transform.Find("Background/SliderColony").GetComponent<Slider>().enabled = false;
+        this.transform.Find("Background/SliderColony/MinSlideText").GetComponent<Text>().enabled = false;
+        this.transform.Find("Background/SliderColony/MaxSlideText").GetComponent<Text>().enabled = false;
+
     }
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 	
 	}
     //clicking on an object load its specs
-    public void selected(GenericObject selectedObj)
+    public void selectedObj(GenericObject selectedObj)
     {
         if (selectedObject)
             selectedObject.deselect();
@@ -50,12 +61,28 @@ public class Infobar : MonoBehaviour {
          
     }
     //deselecting an object reload main infos
-    public void deselected()
+    public void deselectedObj()
     {
         this.selectedObject = null;
         this.transform.Find("Background/MaterialText").GetComponent<Text>().text = "TERMITES AVAILABLE: " + this.levelInPlay.availableTermites;
         this.transform.Find("Background/TermitesText").GetComponent<Text>().text = "";
         this.transform.Find("Background/IntegrityText").GetComponent<Text>().text = "";
     }
-    //setting boosters
+    //select and deselect colony
+    public void selectedColony(Colony colonyselected)
+    {
+        this.transform.Find("Background/MaterialText").GetComponent<Text>().text = colonyselected.
+        this.transform.Find("Background/TermitesText").GetComponent<Text>().text = "TERMITES: " + selectedObj.counter;
+        this.transform.Find("Background/IntegrityText").GetComponent<Text>().text = "INTEGRITY: " + selectedObj.integrity;
+
+    }
+    public void delesectColony()
+    {
+        this.selectedObject = null;
+        this.transform.Find("Background/MaterialText").GetComponent<Text>().text = "TERMITES AVAILABLE: " + this.levelInPlay.availableTermites;
+        this.transform.Find("Background/TermitesText").GetComponent<Text>().text = "";
+        this.transform.Find("Background/IntegrityText").GetComponent<Text>().text = "";
+    }
+    //set booster on screen by type of the level
+    
 }
