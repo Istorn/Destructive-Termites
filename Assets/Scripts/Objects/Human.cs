ab013bc6381dd39933b189e03db780532e1fc4de
LydiaC <lydia.cilie@gmail.com>
2016-01-10 00:20:08 +0100

    Generalizing Humans to prepare threats incoming
    moving functions from Human to LiveObject and generalizing with some
    parameters in order to have a liveObject adaptable to the threats needs.

--- Assets/Scripts/Objects/Human.cs
+++ Assets/Scripts/Objects/Human.cs
@@ -4,114 +4,42 @@ using System.Collections.Generic;
 
 public class Human : LiveObject {
 
-    private float alertPercentage = 0f;
-    public int actualNodeNumber = 0;
-    private bool isAttacking = false;
+	private string sprayAtkAnimStr = "isSprayAttacking";
+	// private string gasAtkAnimStr = "isGasAttacking";
+	// private string bombAtkAnimStr = "isBombAttacking";
+	// private string gunAtkAnimStr = "isGunAttacking";
+	private string walkAnimStr = "isWalking";
+	private string atkAnimStr;
 
     private IEnumerator movementCoroutine = null;
     private IEnumerator attackCoroutine = null;
 
-    private GenericObject targetObject = null;
-
-    private ConcurrentQueue<Graph.Node> movementPath;
     protected override void Awake()
     {
         base.Awake();
         movementPath = new ConcurrentQueue<Graph.Node>();
-        movementCoroutine = movement();
-        attackCoroutine = attackColony();
+		this.atkAnimStr = sprayAtkAnimStr; // defaut attack is spray
+		setMovementCoroutine();
         animator.runtimeAnimatorController = (RuntimeAnimatorController)RuntimeAnimatorController.Instantiate(Resources.Load("Animations/OliAnimatorController"));
     }
 
+	protected void setMovementCoroutine(){
+        movementCoroutine = movement(atkAnimStr,walkAnimStr,Costants.HUMAN_WAIT_TIME,Costants.HUMAN_SPEED);
+	}
+	
+	protected void setAttackCoroutine(){		
+        attackCoroutine = attackColony(); 
+	}
+	
     protected override void move()
     {
-        base.move();
+        base.move(); // calls the father's move method prior to execute the rest of this method
     }
-
 
     void Start()
     {
         StartCoroutine(movementCoroutine);
         StartCoroutine(attackCoroutine);
-    }
-
-    IEnumerator movement()
-    {
-        while (true)
-        {
-            bool move = true;
-            if (movementPath.Count < 2)
-            {
-                if (isAttacking)
-                {
-                    animator.SetBool("isSprayAttacking", true);
-                    if (targetObject.getAttacker())
-                    {
-                        targetObject.getAttacker().attack();
-                        move = false;
-                        yield return new WaitForSeconds(Costants.HUMAN_WAIT_TIME);
-                    }
-                    else
-                    {
-                        animator.SetBool("isWalking", true);
-                        animator.SetBool("isSprayAttacking", false);
-                        isAttacking = false;
-                    }
-                        
-                }
-                if (move)
-                {
-                    movementPath.Clear();
-                    animator.SetBool("isWalking", false);
-                    int endNode = findDestination();
-                    List<Graph.Node> path = GameManager.getCurrentLevel().getGraphLiveObjects().getPath(actualNodeNumber, endNode);
-                    foreach (Graph.Node n in path)
-                        movementPath.Enqueue(n);
-                    yield return new WaitForSeconds(Costants.HUMAN_WAIT_TIME);
-                    animator.SetBool("isWalking", true);
-                }
-            }
-            if (move)
-            {
-                StartCoroutine(stepMovement());
-                yield return new WaitForSeconds(Costants.HUMAN_SPEED);
-            }
-
-        }
-    }
-
-    IEnumerator stepMovement()
-    {
-        Graph.Node start = movementPath.Dequeue();
-        Graph.Node end = movementPath.Peek();
-        actualNodeNumber = end.number;
-        gameObject.GetComponent<SpriteRenderer>().sortingOrder = start.getZIndex(end);
-        int sign = System.Math.Sign(end.coordinates.x - start.coordinates.x) >= 0? 1: -1;
-        transform.localScale = new Vector3(System.Math.Abs(transform.localScale.x) * sign, transform.localScale.y, transform.localScale.z);
-        yield return StartCoroutine(MoveObject(transform, start, end, Costants.HUMAN_SPEED));
-    }
-
-    IEnumerator MoveObject(Transform thisTransform, Graph.Node start, Graph.Node end, float time)
-    {
-        var i = 0.0f;
-        var rate = 1.0f / time;
-        while (i < 1.0f)
-        {
-            i += Time.deltaTime * rate;
-            transform.position = Vector3.Lerp(start.coordinates, end.coordinates, i);
-            yield return null;
-        }
-    }
-
-    private int findDestination()
-    {
-        int endNode = 0;
-        do
-        {
-            endNode = Random.Range(0, GameManager.getCurrentLevel().getGraphLiveObjects().nodes.Count);
-        }
-        while ((endNode == actualNodeNumber) || (GameManager.getCurrentLevel().getGraphLiveObjects().findNode(endNode).type.Equals(Graph.Node.Type.Generic)));
-        return endNode;
     }
 
     private IEnumerator attackColony()
@@ -121,8 +49,8 @@ public class Human : LiveObject {
             if (GameManager.getCurrentLevel().alertObjectsQueue.Count > 0 && !isAttacking)
             {
                 isAttacking = true;
-                StopCoroutine(movementCoroutine);
-                movementCoroutine = movement();
+                StopCoroutine(movementCoroutine);//pointer to movementCoroutine goes null
+				setMovementCoroutine();
                 targetObject = (GenericObject)GameManager.getCurrentLevel().alertObjectsQueue.Dequeue();
                 int target = GameManager.getCurrentLevel().getGraphLiveObjects().findNearestNode(targetObject.getRoom().number, targetObject.gameObject.transform.position);
                 List<Graph.Node> path = GameManager.getCurrentLevel().getGraphLiveObjects().getPath(actualNodeNumber, target);
--- Assets/Scripts/Objects/LiveObject.cs
+++ Assets/Scripts/Objects/LiveObject.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LiveObject : GenericObject {
 
     protected Animator animator;
-
     protected bool isMoving = false;
-
+    protected bool isAttacking = false;
+	protected Colony target;
+    protected GenericObject targetObject = null;	
+    protected const float alertLevelThreshold = 0.1F; // IS IN RANGE 0-1.	
+    protected float alertPercentage = 0f;	
+    public int actualNodeNumber = 0;
+    protected ConcurrentQueue<Graph.Node> movementPath;
+	
     protected override void Awake()
     {
         base.Awake();
@@ -15,7 +22,95 @@ public class LiveObject : GenericObject {
         animator = gameObject.AddComponent<Animator>();
     }
 
-    protected virtual void move()
-    {
+    protected virtual void move(){
     }
+	
+	protected IEnumerator movement(string AtkAnimStr, string walkAnimStr, float objectWaitingTime, float objectSpeed){
+        while (true)
+        {
+            bool move = true;
+            if (movementPath.Count < 2)
+            {
+                if (isAttacking)
+                {
+                    animator.SetBool(AtkAnimStr, true);
+                    if (targetObject.getAttacker())
+                    {
+                        targetObject.getAttacker().attack();
+                        move = false;
+                        yield return new WaitForSeconds(objectWaitingTime);
+                    }
+                    else
+                    {
+                        animator.SetBool(walkAnimStr, true);
+                        animator.SetBool(AtkAnimStr, false);
+                        isAttacking = false;
+                    }
+                }
+                if (move)
+                {
+                    movementPath.Clear();
+                    animator.SetBool(walkAnimStr, false);
+                    int endNode = findDestination();
+                    List<Graph.Node> path = GameManager.getCurrentLevel().getGraphLiveObjects().getPath(actualNodeNumber, endNode);
+                    foreach (Graph.Node n in path)
+                        movementPath.Enqueue(n);
+                    yield return new WaitForSeconds(objectWaitingTime);
+                    animator.SetBool(walkAnimStr, true);
+                }
+            }
+            if (move)
+            {
+                StartCoroutine(stepMovement());
+                yield return new WaitForSeconds(objectSpeed);
+            }
+        }
+    }
+	
+	private int findDestination(){
+        int endNode = 0;
+        do
+        {
+            endNode = Random.Range(0, GameManager.getCurrentLevel().getGraphLiveObjects().nodes.Count);
+        }
+        while ((endNode == actualNodeNumber) || (GameManager.getCurrentLevel().getGraphLiveObjects().findNode(endNode).type.Equals(Graph.Node.Type.Generic)));
+        return endNode;
+    }
+	
+	IEnumerator stepMovement(){
+        Graph.Node start = movementPath.Dequeue();
+        Graph.Node end = movementPath.Peek();
+        actualNodeNumber = end.number;
+        gameObject.GetComponent<SpriteRenderer>().sortingOrder = start.getZIndex(end);
+        int sign = System.Math.Sign(end.coordinates.x - start.coordinates.x) >= 0? 1: -1;
+        transform.localScale = new Vector3(System.Math.Abs(transform.localScale.x) * sign, transform.localScale.y, transform.localScale.z);
+        yield return StartCoroutine(MoveObject(transform, start, end, Costants.HUMAN_SPEED));
+    }
+	
+	IEnumerator MoveObject(Transform thisTransform, Graph.Node start, Graph.Node end, float time){
+        var i = 0.0f;
+        var rate = 1.0f / time;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            transform.position = Vector3.Lerp(start.coordinates, end.coordinates, i);
+            yield return null;
+        }
+    }
+	
+	public Colony getTarget(){
+		return target;
+	}
+	
+	private void setTarget(Colony col){
+		this.target = col;
+	}
+	
+	private float getAlertLevel(){
+		// int destroyable = nbDestroyableItems;
+		// return (destroyable /(destroyable+nbDestroyedItems) ); // 0 if house destroyed, 1 if house new
+		
+		int destroyable = 5;
+		return (destroyable /(destroyable+1) ); // 0 if house destroyed, 1 if house new
+	}
 }
